79f05cafa20f87256c796dd70285224269d87e5c
You Ji <yoj@microsoft.com>
2021-02-03 10:34:55 +0800

    update code
    Signed-off-by: You Ji <yoj@microsoft.com>

--- 08.BinarySearch/T1560.java
+++ 08.BinarySearch/T1560.java
@@ -3,20 +3,33 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-  static private String binarySearch(long[] A, int n, long target) {
-      int l = 0, r = n;
-      while (l < r) {
-          final int m = l + ((r-l)>>1);
-          if (A[m] == target) {
-              return new String("YES");
-          }
-          if (A[m] < target) {
-              l = m + 1;
-          } else {
-              r = m;
-          }
+  static private boolean binarySearch(long[] A, long target) {
+    if (A == null || A.length == 0) {
+      return false;
+    }
+    // 首先设定初始区间，这里我们采用开闭原则[l, r)
+    int l = 0, r = A.length;
+    // 循环结束的判断条件是整个区间为空区间，那么运行结束。
+    // 我们使用的是开闭原则来表示一个区间，所以当l < r的时候
+    // 我们要查找的区间还不是一个空区间。
+    while (l < r) {
+      final int m = l + ((r - l) >> 1);
+      if (A[m] == target) {
+        return true;
       }
-      return new String("NO");
+      if (A[m] < target) {
+        // 当中点比目标值小时，需要把左边的部分扔掉。即[l, m]
+        // 这个区间扔掉，由于我们采用的是开闭原则，所以新的区间需要是
+        // [m + 1, r), 因引需要将l = m + 1
+        l = m + 1;
+      } else {
+        // 当中点比目标值大时，需要把右边的部分扔掉，即[m, r)这个区间扔掉。
+        // 那么新区间变成[l, m)。由于我们使用的是开闭原则，
+        // 只需要设置r = m即可。
+        r = m;
+      }
+    }
+    return false;
   }
 
   public static void main(String args[]) throws Exception {
@@ -30,8 +43,8 @@ public class Main {
     }
     Arrays.sort(A);
     for (int i = 0; i < m; i++) {
-        long target = sc.nextLong();
-        System.out.println(binarySearch(A, n, target));
+      long target = sc.nextLong();
+      System.out.println(binarySearch(A, target) ? "YES" : "NO");
     }
   }
 }

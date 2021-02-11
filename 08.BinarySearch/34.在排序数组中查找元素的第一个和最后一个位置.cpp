f64c2be89a73690480ec6cc577557738add5647d
You Ji <yoj@microsoft.com>
2021-02-11 09:09:22 +0800

    update code format
    :
    
    Signed-off-by: You Ji <yoj@microsoft.com>

--- 08.BinarySearch/34.在排序数组中查找元素的第一个和最后一个位置.cpp
+++ 08.BinarySearch/34.在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -6,44 +6,48 @@
 
 // @lc code=start
 class Solution {
-    template<typename RandomIterator, typename ValueType>
-    RandomIterator _lower_bound(RandomIterator first, RandomIterator last, ValueType v) {
-        if (first == last) return first;
-        while (first != last) {
-            const size_t d = distance(first, last);
-            auto m = next(first, d>>1);
-            if (*m < v) {
-                first = next(m, 1);
-            } else {
-                last = m;
-            }
-        }
-        return first;
+  template <typename RandomIterator, typename ValueType>
+  RandomIterator _lower_bound(RandomIterator first, RandomIterator last,
+                              ValueType v) {
+    if (first == last)
+      return first;
+    while (first != last) {
+      const size_t d = distance(first, last);
+      auto m = next(first, d >> 1);
+      if (*m < v) {
+        first = next(m, 1);
+      } else {
+        last = m;
+      }
     }
+    return first;
+  }
 
-    template<typename RandomIterator, typename ValueType>
-    RandomIterator _upper_bound(RandomIterator first, RandomIterator last, ValueType v) {
-        if (first == last) return first;
-        while (first != last) {
-            auto d = distance(first, last);
-            auto m = next(first, d>>1);
-            if (*m <= v) {
-                first = m + 1;
-            } else {
-                last = m;
-            }
-        }
-        return first;
+  template <typename RandomIterator, typename ValueType>
+  RandomIterator _upper_bound(RandomIterator first, RandomIterator last,
+                              ValueType v) {
+    if (first == last)
+      return first;
+    while (first != last) {
+      auto d = distance(first, last);
+      auto m = next(first, d >> 1);
+      if (*m <= v) {
+        first = m + 1;
+      } else {
+        last = m;
+      }
     }
+    return first;
+  }
+
 public:
-    vector<int> searchRange(vector<int>& A, int T) {
-        int l = _lower_bound(A.begin(), A.end(), T) - A.begin();
-        if (l == A.size() || A[l] != T) {
-            return {-1,-1};
-        }
-        int r = _upper_bound(A.begin() + l, A.end(), T) - A.begin();
-        return {l, r-1};
+  vector<int> searchRange(vector<int> &A, int T) {
+    int l = _lower_bound(A.begin(), A.end(), T) - A.begin();
+    if (l == A.size() || A[l] != T) {
+      return {-1, -1};
     }
+    int r = _upper_bound(A.begin() + l, A.end(), T) - A.begin();
+    return {l, r - 1};
+  }
 };
 // @lc code=end
-
